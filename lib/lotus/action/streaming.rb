0fdd17397e3e8abbec042f7ad41f5f6bd264f4d1
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2014-08-06 21:31:47 -0300

    [WIP]: move out the responsability to treat the type of transport from #stream.

--- lib/lotus/action/streaming.rb
+++ lib/lotus/action/streaming.rb
@@ -8,13 +8,8 @@ module Lotus
       end
 
       class SSE
-        def initialize(app)
-          @app = app
-        end
-
-        def open
-          @app.headers["Content-Type"] = "..."
-        end
+        def content_type; 'text/event-stream' end
+        def format; :sse end
 
         def call(message, options)
           # transform the message here
@@ -26,10 +21,30 @@ module Lotus
       end
 
       protected
-      def stream(transport = :sse, &blk)
-        # decides which transport to use
-        # can receive an object that respondo_to? :transform
-        # the stream knows to call #transport before write a message
+
+      def stream(transport = SSE.new, &blk)
+        raise 'invalid transport' unless transport.respond_to? :call
+
+        self.headers['Cache-Control'] = 'no-cache'
+        self.headers['Content-Type']  = transport.content_type
+
+        self.format = transport.format
+        if @_env['async.callback']
+          evented_stream transport
+        else
+          threaded_stream transport
+        end
+      end
+
+      private
+
+      def evented_stream(transport)
+        response = [ 200, { 'Content-Type' => transport.content_type }, [] ]
+        @_env['async.callback'].call response
+        throw :async
+      end
+
+      def threaded_stream(transport)
       end
     end
   end
