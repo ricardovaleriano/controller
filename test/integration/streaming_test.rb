1499b3756f44643379cbfcc9ae03c19dde4dacbe
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2014-08-14 09:04:43 -0300

    [WIP]: warning, failing tests.
    Note to self on how to test blocking streaming
    Note to self to not forget the multiline strings.

--- test/integration/streaming_test.rb
+++ test/integration/streaming_test.rb
@@ -1,4 +1,7 @@
 require 'test_helper'
+require 'lotus/action/streaming'
+
+require 'countdownlatch'
 
 describe 'Streaming actions' do
   class StreamingAction
@@ -9,7 +12,7 @@ describe 'Streaming actions' do
       stream do |out|
         out.write 'starting'
         out.write 'streaming'
-        out.write 'good bye!'
+        out.write 'goodbye!'
         out.close
       end
     end
@@ -27,23 +30,6 @@ describe 'Streaming actions' do
     end
   end
 
-  class BlockingStreaming
-    include Lotus::Action
-    include Lotus::Action::Streaming
-
-    attr_accessor :latch
-
-    def call(params)
-      blocking_stream do |out|
-        out.write 'starting'
-        out.write 'gonna block...'
-        latch.wait
-        out.write 'good bye!'
-        out.close
-      end
-    end
-  end
-
   class LeftOpenStreaming
     include Lotus::Action
     include Lotus::Action::Streaming
@@ -52,7 +38,7 @@ describe 'Streaming actions' do
       stream do |out|
         out.write 'starting'
         out.write 'streaming'
-        out.write 'good bye!'
+        out.write 'goodbye!'
       end
     end
   end
@@ -68,10 +54,22 @@ describe 'Streaming actions' do
     end
   end
 
+  class StreamingMultilineString
+    include Lotus::Action
+    include Lotus::Action::Streaming
+
+    def call(params)
+      stream do |out|
+        out.write "wow\nsuch a message\nmuch multilined"
+        out.close
+      end
+    end
+  end
+
   describe 'Non blocking ("normal") stream' do
     it 'receives the response streamed in the stream block' do
       response = Rack::MockRequest.new(StreamingAction.new).get('/')
-      response.body.must_equal "data: starting\n\ndata: streaming\n\ndata: good bye!\n\n"
+      response.body.must_equal "data: starting\n\ndata: streaming\n\ndata: goodbye!\n\n"
     end
 
     it 'forces the right (default) content type header' do
@@ -89,7 +87,7 @@ describe 'Streaming actions' do
 
     describe 'defaults the transport to server sent events' do
       before do
-        @action = Rack::MockRequest.new(StreamingActionWithOptions.new)
+        @action = Rack::MockRequest.new StreamingActionWithOptions.new
       end
 
       it 'send the id' do
@@ -107,12 +105,41 @@ describe 'Streaming actions' do
         response.body.must_equal "retry: 10000\ndata: starting\n\n"
       end
 
-      it 'send a multiline string as data'
-      it 'just ignores unknown options'
+      it 'just ignores unknown options' do
+        response = @action.get('/', params: { omg_not_sse: 'invalid' })
+        response.body.must_equal "data: starting\n\n"
+      end
+
+      it 'send a multiline string as data' do
+        action = Rack::MockRequest.new StreamingMultilineString.new
+        response = action.get('/')
+        response.body.must_equal "data: wow\ndata: such a message\n data: much multilined\n\n"
+      end
+    end
+  end
+
+  class BlockingStreaming
+    include Lotus::Action
+    include Lotus::Action::Streaming
+
+    def initialize
+    end
+
+    def call(params)
+      blocking_stream do |out|
+        # I'm in a new thread, external latch release
+        out.write 'starting'
+        out.write 'gonna block...'
+        # Internal latch block, to be released later outside
+        #   after the assertion to be sure that two writes have been made.
+        #   after release, check if the final write is made.
+        out.write 'goodbye!'
+        out.close
+      end
     end
   end
 
   describe 'blocking stream' do
-
+    it 'send response chuncks in a background thread'
   end
 end
